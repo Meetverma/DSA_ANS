f2d2b8ba62a144c81f1afd4291baace2a2a9b8b8
Meet Verma <109876335+Meetverma@users.noreply.github.com>
2024-07-07 22:35:42 +0530

    Create 1518_Water_bottles.java

---
+++ LeetCode/1518_Water_bottles.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int ans=0 ;
+        while(numBottles >= numExchange ){
+            ans += numExchange;
+            numBottles -= numExchange;
+
+            numBottles++;
+        }
+        return ans + numBottles ;
+    }
+}
