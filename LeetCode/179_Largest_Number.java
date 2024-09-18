d4c1c283d6ced106a599c3f1b86b9141189e2273
Meet Verma <109876335+Meetverma@users.noreply.github.com>
2024-09-18 18:46:15 +0530

    Create 179_Largest_Number.java

---
+++ LeetCode/179_Largest_Number.java
@@ -0,0 +1,30 @@
+
+public class Solution {
+    public String largestNumber(int[] nums) {
+
+        String[] strNums = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            strNums[i] = String.valueOf(nums[i]);
+        }
+        
+        Arrays.sort(strNums, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                String order1 = a + b;
+                String order2 = b + a;
+                return order2.compareTo(order1); 
+            }
+        });
+        
+        if (strNums[0].equals("0")) {
+            return "0";
+        }
+        
+        StringBuilder largestNumber = new StringBuilder();
+        for (String num : strNums) {
+            largestNumber.append(num);
+        }
+        
+        return largestNumber.toString();
+    }
+}
